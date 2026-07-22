e86af18896404f5a95963e40ee157de93b242336
Flavio Nadler <flavio.nadler@asinfotrack.ch>
2026-07-22 22:42:37 +0200

    implement scroll behaviour

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,21 @@ import Sources from '@/views/legal/Sources.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      }
+    }
+
+    return {
+      top: 0,
+      left: 0,
+    }
+  },
+
   routes: [
     {
       path: '/',
