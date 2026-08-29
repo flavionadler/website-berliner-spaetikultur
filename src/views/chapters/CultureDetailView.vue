6a35a248defd3f9f5a33810c2c55d91b5419e95c
Marko-5400 <marko.akrap07@gmail.com>
2026-08-29 21:49:00 +0200

    Add base structure to culture page

--- src/views/chapters/CultureDetailView.vue
+++ src/views/chapters/CultureDetailView.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-
 import HeroSection from '@/components/HeroSection.vue';
 import heroImg from '@/assets/images/hero/culture.jpeg';
+import PageContainer from '@/components/PageContainer.vue';
+import ContentSection from '@/components/ContentSection.vue';
 </script>
 
 <template>
 	<HeroSection :image="heroImg" title="Kultur und Soziales" eyebrow="Spätis als Treffpunkte"
-	             description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam" />
+	             description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
+	             scroll-target="#culture-content" />
+	<PageContainer id="culture-content">
+		<ContentSection>
+		</ContentSection>
+	</PageContainer>
 </template>
 
 <style scoped>
