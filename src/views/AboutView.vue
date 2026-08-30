40989f291c24e17b8bc28ea202b9c17e98f356b3
Flavio Nadler <flavio.nadler@asinfotrack.ch>
2026-08-30 20:56:39 +0200

    implement about view with placeholders

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
 
+import HeroSection from '@/components/HeroSection.vue';
+import heroImg from '@/assets/images/hero/home.jpeg';
+import PageContainer from '@/components/PageContainer.vue';
+import ContentSection from '@/components/ContentSection.vue';
 </script>
 
 <template>
+	<HeroSection title="Über das Projekt" :image="heroImg" eyebrow="Über uns und das Projekt"
+	             scroll-target="#about-content"/>
+	<PageContainer id="about-content">
+		<ContentSection title="Das Projekt">
+			<p>Beschreibung was das Projekt ist, warum gwählt, Leitfrage, These und Beantwortung</p>
+		</ContentSection>
 
+		<ContentSection title="Vorgehen">
+			<p>Beschreibung des Vorgehen, Reise nach Berlin usw.</p>
+		</ContentSection>
+
+		<ContentSection title="Danksagungen">
+			<p>Danke an alle Interviewpartner</p>
+		</ContentSection>
+	</PageContainer>
 </template>
 
 <style scoped>
